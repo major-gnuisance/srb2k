7be6af02beeb6170bc0ae1dedbfce1799a96475c
Ashnal <ashnal52@gmail.com>
2020-04-11 13:13:37 -0400

    afk timer improvements adds a cvar to allow admins to ignore the auto spec too if desired little more clear coding, replace 0 with serverplayer

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -399,8 +399,9 @@ consvar_t cv_kartdebugcolorize = {"kartdebugcolorize", "Off", CV_NOSHOWHELP, CV_
 //Does not affect the host
 static CV_PossibleValue_t afktimer_cons_t[] = {{30, "MIN"}, {INT32_MAX, "MAX"}, {0, NULL}}; //In seconds
 consvar_t cv_afkspectimer = {"afkspectimer", "300", CV_SAVE, afktimer_cons_t, NULL, 0, NULL, NULL, 0, 0, NULL}; //Very long defaults to not upset anyone
+consvar_t cv_afkspecignoreadmins = {"afkspecignoreadmins", "Off", CV_SAVE|CV_NOSHOWHELP, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL};
 consvar_t cv_afkkicktimer = {"afkkicktimer", "600", CV_SAVE, afktimer_cons_t, NULL, 0, NULL, NULL, 0, 0, NULL}; //Server owners can tighten or loosen these
-consvar_t cv_afkkickignoreadmins = {"afkkickignoreadmins", "On", CV_SAVE|CV_NETVAR|CV_NOSHOWHELP, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL};
+consvar_t cv_afkkickignoreadmins = {"afkkickignoreadmins", "On", CV_SAVE|CV_NOSHOWHELP, CV_OnOff, NULL, 0, NULL, NULL, 0, 0, NULL};
 
 static CV_PossibleValue_t votetime_cons_t[] = {{10, "MIN"}, {3600, "MAX"}, {0, NULL}};
 consvar_t cv_votetime = {"votetime", "20", CV_SAVE|CV_NETVAR, votetime_cons_t, NULL, 0, NULL, NULL, 0, 0, NULL};
@@ -743,6 +744,7 @@ void D_RegisterServerCommands(void)
 	CV_RegisterVar(&cv_pingtimeout);
 	CV_RegisterVar(&cv_showping);
 	CV_RegisterVar(&cv_afkspectimer);
+	CV_RegisterVar(&cv_afkspecignoreadmins);
 	CV_RegisterVar(&cv_afkkicktimer);
 	CV_RegisterVar(&cv_afkkickignoreadmins);
 
