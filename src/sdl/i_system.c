d209f7ab0792665deaa563ae2c014c7b6e3207a3
Hannu Hanhi <hhanhipublic@gmail.com>
2020-06-02 20:08:30 +0300

    Add SDLTICKS compilation option to use SDL_GetTicks time function on Unix

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -2998,6 +2998,22 @@ static void I_ShutdownTimer(void)
 	}
 }
 #else
+
+#ifdef SDLTICKS
+static int TimeMillis(void)
+{
+	static Uint64 basetime = 0;
+		   Uint64 ticks = SDL_GetTicks();
+
+	if (!basetime)
+		basetime = ticks;
+
+	ticks -= basetime;
+
+	return (int)ticks;
+}
+#else
+
 #ifndef __MACH__
 struct timespec clk_basetime;
 
@@ -3016,6 +3032,8 @@ static int TimeMillis(void)
 	return ms;
 }
 #endif/*__MACH__*/
+
+#endif/*SDLTICKS*/
 
 #endif
 
